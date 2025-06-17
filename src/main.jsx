b5e8017ffbf66498a1fe391e8aecd2ef4f2bdf2a
JJ-b2 <jack2022mgk@gmail.com>
2025-06-18 01:03:15 +0800

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import GlowRoadLandingPage from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './pages/App.jsx';
+import About from './pages/About.jsx';
+import Products from './pages/Products.jsx';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <GlowRoadLandingPage />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/products" element={<Products />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
-)
+);
